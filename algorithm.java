11a82f4e1cbe06b99971e76ef64b6ae36b153efe
ecebarani <34707583+ecebarani@users.noreply.github.com>
2017-12-21 22:33:00 +0530

    Add files via upload

---
+++ algorithm.java
@@ -0,0 +1,59 @@
+			1..Find out the number entered by the user is divisible by 5
+
+
+step 1:start.
+step :declare the value of i and count as 0.
+step 3:check the condition true or false.
+step 4:check i is divisible by 5.
+step 5:if yes, print i & increase the count.
+step 6:when condition gets false,loop gets exit.
+step 7:display the number divisible by 5.
+step 8:stop.
+
+			2..Display first 10 multiples of 9
+
+step 1:start.
+step 2:declare the value of num=9.
+step 3:declare the value of result=0.
+step 4:check the condition as(i=0;i<=10;i++).
+step 5:if condition true,num*i=result.
+step 6:if false,loop get exit.
+step 7:display the output.
+step 8:stop.
+
+			3..find wheather the number entered by the user is odd or even
+
+step 1:start.
+step 2:read num.
+step 3:if num divisible by 2.
+step 4:display even.
+step 5:if not display odd.
+step 6:stop.
+
+			4..algorithm to accept 2 numbers from the users,subract the 1st number by 2nd number
+
+step 1:start.
+step 2:get the two value from the user.
+step 3:where the first value is greater than the second value.
+step 4:declare first value as a, and second value as b. 
+step 5:subract the first value from the second value.
+step 6:stop.
+
+			5..get the number from the user and display in cube
+
+step 1:start.
+step 2:get the value from the user as z.
+step 3:multiply the value of z for 3 times.
+step 4:display the result.
+step 5:stop.
+
+			6..find the factorial of the number
+
+step 1:start.
+step 2:read the numbers.
+step 3:set factorial=1,i=1.
+step 4:check the condition i<=number.if false go to step 7.
+step 5:fact=fact*i
+step 6:display i=i+1,then go to step 4.
+step 7:display fact.
+step 8:stop.
