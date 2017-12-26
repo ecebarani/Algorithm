08444c1318de1c974f1c5e9ea03b018e0ccb1bfb
ecebarani <34707583+ecebarani@users.noreply.github.com>
2017-12-26 15:25:05 +0530

    Add files via upload

--- algorithm.java
+++ algorithm.java
@@ -1,36 +1,72 @@
-			1..Find out the number entered by the user is divisible by 5
+			2..
 
+A COMPILER is a special program that converts the programming language int the machine language,at one go.
+
+			3..atm withdrawl
 
 step 1:start.
-step :declare the value of i and count as 0.
-step 3:check the condition true or false.
-step 4:check i is divisible by 5.
+step 2:go to any local atm.
+step 3:insert/swipe your atm.
+step 4:press the language button that you want to choose.
+step 5:enter the atm pin.
+step 6:press the account type.
+step 7:press the cash withdrawal button.
+step 8:enter the amount you want to withdraw.
+step 9:collect the amount from the atm machine.
+step 10:collect the atm card.
+step 11:stop.
+
+			4..display sum and product of the 2 numbers
+
+step 1:start.
+step 2:read the value of A and B.
+step 3:sum=A+B.
+step 4:display sum.go to step 7.
+step 5:product=A*B.
+step 6:display product.go to step 7.
+step 7:stop.
+
+			5..increment of given number by 1
+
+steo 1:start.
+step 2:declare num.
+step 3:accept the value for number.
+step 4:increament number by adding 1.
+step 5:print the result.
+step 6:stop.
+
+			6..Find out the number entered by the user is divisible by 5
+
+step 1:start.
+step 2:declare the value of i.
+step 3:check i is divisible by 5.
+step 4:check the condition true or false.
 step 5:if yes, print i & increase the count.
-step 6:when condition gets false,loop gets exit.
+step 6:when condition gets false,loop gets exist.
 step 7:display the number divisible by 5.
 step 8:stop.
 
-			2..Display first 10 multiples of 9
+			7..Display first 10 multiples of 9
 
 step 1:start.
 step 2:declare the value of num=9.
 step 3:declare the value of result=0.
 step 4:check the condition as(i=0;i<=10;i++).
 step 5:if condition true,num*i=result.
-step 6:if false,loop get exit.
+step 6:if false,loop get exist.
 step 7:display the output.
 step 8:stop.
 
-			3..find wheather the number entered by the user is odd or even
+			8..find wheather the number entered by the user is odd or even
 
 step 1:start.
 step 2:read num.
-step 3:if num divisible by 2.
+step 3:if remainder became 0.
 step 4:display even.
 step 5:if not display odd.
 step 6:stop.
 
-			4..algorithm to accept 2 numbers from the users,subract the 1st number by 2nd number
+			9..algorithm to accept 2 numbers from the users,subract the 1st number by 2nd number
 
 step 1:start.
 step 2:get the two value from the user.
@@ -39,7 +75,17 @@ step 4:declare first value as a, and second value as b.
 step 5:subract the first value from the second value.
 step 6:stop.
 
-			5..get the number from the user and display in cube
+			10..algorithm for principle,rate,time and display the simple intrest
+
+step 1:start.
+step 2:enter the principle value.
+step 3:enter the rate value.
+step 4:enter the time value.
+step 5:intrest=(p.t.r)/100.
+step 6:display the value.
+step 7:stop.
+
+			11..get the number from the user and display in cube
 
 step 1:start.
 step 2:get the value from the user as z.
@@ -47,7 +93,78 @@ step 3:multiply the value of z for 3 times.
 step 4:display the result.
 step 5:stop.
 
-			6..find the factorial of the number
+			12..algorithm for to display area and perimeter of rectangle
+
+step 1:start.
+step 2:accept the length of L.
+step 3:accept the breadth of B.
+step 4:compute the area of rectangle, A=L*B.
+step 5:compute the perimeter of rectangle, P=2(L+B).
+step 6:display the result.
+step 7:stop.
+
+			13..remarks should be given to the students
+
+step 1:start.
+step 2:get the three marks m1,m2,m3 from user.
+step 3:add 3 marks stored in value tot.
+step 4:if tot is greater than 90 and less than 100 print grade A.
+step 5:if tot is greater than 80 and less than 90 print grade B.
+step 6:if tot is greater than 70 and less than 80 print grade C.
+step 7:end
+
+			14..sum of first 10 n natural numbers
+
+step 1:start.
+step 2:print 1
+step 3:print 2
+step 4:print 3
+step 5:print 4
+step 6:print 5
+step 7:print 6
+step 8:print 7
+step 9:print 8
+step 10:print 9
+step 11:print 10
+step 12:stop
+
+			15..pseudocode for sum of 1st 10 natural numbers
+
+begin
+    numeric i,n1,number=10
+	for(i=1;i<=number;i++)
+	begin 
+	    display i
+	end
+end
+
+			16..display the table of 12
+
+step 1:start.
+step 2:multiply 1*12 & print result
+step 3:multiply 2*12 & print result
+step 4:multiply 3*12 & print result
+step 5:multiply 4*12 & print result
+step 6:multiply 5*12 & print result
+step 7:multiply 6*12 & print result
+step 8:multiply 7*12 & print result
+step 9:multiply 8*12 & print result
+step 10:multiply 9*12 & print result
+step 11:multiply 10*12 & print result
+step 12:stop
+
+			17..pseudocode for display the tables of 12
+
+begin
+    numeric num=12, i, result
+	for(i=1;i<=10;i++)
+	begin
+	    result= i*12
+		display result
+	end
+end
+	  
+			18..find the factorial of the number
 
 step 1:start.
 step 2:read the numbers.
@@ -57,3 +174,194 @@ step 5:fact=fact*i
 step 6:display i=i+1,then go to step 4.
 step 7:display fact.
 step 8:stop.
+
+			19..pseudocode for factorial of given number
+
+begin
+    numeric fact=0, i
+	for(i=0;i<5;i++)
+	begin
+	    fact= fact*i
+	end
+	display fact
+end
+
+			20..pseudocode for accept a number positive ,negative or zero
+
+begin
+    numeric n
+	display 'accept the value for n'
+	accept n
+	
+	if(n>0)
+	begin
+	    display 'the number is positive'
+	end
+	
+	else if(n<0)
+	    display 'the number is negative'
+	end
+	
+	else
+	    display 'the number is zero'
+	end
+end
+    
+
+			22..pseudocode for fibonacci series
+
+begin
+    numeric n1, n2, n3, i
+	n1=0, n2=1
+	display n1
+	display n2
+	for(i=0;i<5;i++)
+	begin
+	    n3=n1+n2
+		display n3
+		n1=n2
+		n2=n3
+	end
+end
+
+
+			24..pseudocode for accept 2 numbers and display sum and product
+
+begin 
+    numeric num1, num2, sum, product
+	display 'enter the value of num1'
+	accept num1
+	display 'enter the value of num2'
+	accept num2
+	sum= num1+num2
+	product= num1*num2
+	display sum
+	display product
+end
+
+
+ 			26..pseudocode for celsius convert into fahrenheit
+
+begin 
+    numeric celsius
+	display 'enter the value for celsius'
+    accept celsius
+    compute result= (9/5)*celsius+32	
+	display result
+end
+
+			27..pseudocode for equal or not from user
+
+begin
+    numeric n1, n2
+	display 'enter the value for n1'
+	accept n1
+	display 'enter the value for n2'
+	accept n2
+	if(n1==n2)
+	    begin
+	        display 'they are equal'
+	    end
+	
+	else
+	    begin
+		    display 'they are not equal'
+	    end
+end
+
+			29..Write a pseudocode, where the discount percentage on a TV needs to be decided.
+
+  ->start
+-> string crt,lcd,tv
+->numeric inch 
+->display model of tv
+ ->accept tv
+->Display inch of tv
+  ->accept inch
+->if (tv==crt)display 5% discount of selling price
+->else if(inch==14)display 8% discount of selling price
+->else if (inch==21)display 10% discount of selling price
+->else display 'enter the correct model'
+->end
+
+			30..pseudocode, where all candidates have to take two tests before appearing for an interview. A candidate is selected for the interview round
+
+->start
+   ->numeric test1,test2
+  ->display the mark of test1
+->accept test1
+->display the mark of test2
+->accept test2
+->if (test1>=75&&test2>=75)display scored good marks and send a letter
+->else if(test>=80&&test2>=80) display scored marks and send a letter
+->else display better luck a next time
+->end
+
+
+			31..pseudocode for to accept two numbers
+
+
+begin
+    numeric n1, n2, result, choice
+	display 'the value of n1'
+	accept n1
+	display 'the value of n2'
+	accept n2
+	display 'enter the choice'
+	accept choice
+    display '1 for +, 2 for -, 3 for *,4 for /'
+	
+	switch(choice)
+	    begin
+		    case 1:
+			    result= n1+n2
+				display 'result'
+                break
+            	    case 2:
+			    result= n1-n2
+				display 'result'				
+                break
+            	    case 3:
+			    result= n1*n2
+				display 'result'				
+                break
+            	    case 4:
+			    result= n1/n2
+				display 'result'				
+                break
+        end
+end
+
+			33..pseudocode for accept a letter and check its a vowel or consonent.
+
+begin
+    character choice
+	display 'Enter the character to be checked'
+	accept choice
+    switch(choice)
+	    begin
+		    case A: display 'the entered character is vowel'
+			break
+		    case a: display 'the entered character is vowel'
+			break
+			case E: display 'the entered character is vowel'
+			break
+			case e: display 'the entered character is vowel'
+			break
+			case I: display 'the entered character is vowel'
+			break
+			case i: display 'the entered character is vowel'
+			break
+			case O: display 'the entered character is vowel'
+			break
+			case o: display 'the entered character is vowel'
+			break
+			case U: display 'the entered character is vowel'
+			break
+			case U: display 'the entered character is vowel'
+			break
+			
+			default: display 'the entered character is not a vowel'
+		end
+end
+		
